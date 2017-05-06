bec4058ffd870bf55169badad6fb3af37804dffc
MichaelCook <michael@evoluted.net>
2017-05-06 18:03:15 +0100

    Serve up a static page in a nice ES6 kinda way

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-// Server requirements
-let express = require('express'),
-	app = express(),
-	http = require('http').createServer(app);
-app.use(express.static('public'));
+import path from 'path';
+import { Server } from 'http';
+import Express from 'express';
 
-// Server port and IP
-// Don't forget to run:
-//   sudo ufw allow 1112/tcp
-// on the server to allow access.
-// To show all ports that are open:
-//   sudo ufw show added
-let port = 1111,
-	ip = "127.0.0.1";
+// Create express app and server
+const app = new Express();
+const server = new Server(app);
 
-http.listen(port, ip);
-console.log("Server running on "+ip+":"+port);
+// Always return index page (SPA)
+app.get('*', (req, res) => {
+  return res.sendFile(path.join(__dirname + '/src/static/index.html'));
+});
+
+// Start the server
+// Don't forget to allow access if running from a proper server:
+//   sudo ufw allow 1111/tcp
+// To show all ports that are open: sudo ufw show added
+const port = process.env.PORT || 1111;
+const env = process.env.NODE_ENV || 'production';
+server.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.info(`Server running on http://localhost:${port} [${env}]`);
+});
